6d4e388fdadb79803d5a3218974d4729dd7b3fac
IdaUgilt <idawennergaard@gmail.com>
2023-05-11 12:45:42 +0200

    Update

--- update_csv.py
+++ update_csv.py
@@ -4,7 +4,7 @@ import pandas as pd
 from os import listdir, remove
 
 # Create pandas dataframe from metadata file
-df = pd.read_csv('metadata_withmasks.csv')
+df = pd.read_csv('metadata.csv')
 
 # List of mask names
 mask_ids = listdir('test_images_masks')
@@ -15,6 +15,10 @@ for i, row in df.iterrows():
     mask_id = im_id[:-4] + '_mask.png'
     if mask_id in mask_ids:
         df.at[i, 'mask'] = 1
+    else:
+        df.at[i, 'mask'] = 0
+
+df['mask']=df['mask'].astype('int')
 
 # Remove old metadata file and write new one
 remove('metadata_withmasks.csv')
