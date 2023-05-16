ea6fdf1e43715d919fa767b93a4fa4fe61fb20d3
IdaUgilt <idawennergaard@gmail.com>
2023-05-16 19:22:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,4 +18,26 @@ image_folder = 'images' + os.sep
 mask_folder = 'images_masks' + os.sep
 file_features = 'feature_data.csv'
 
+
+
+### PROCESS IMAGES ###
+
+# Extract features for all images with masks and save as csv.
+
 ProcessImages(file_data, image_folder, mask_folder, file_features)
+
+
+### TRAIN MODEL ###
+
+X_train, y_train, X_test, y_test = train_test_split([PAT_ID,features])
+
+clf = train_model(X_train, y_train)
+
+
+### EVALUATE MODEL ###
+
+clf.predict(X_test, y_test)
+clf.predict_proba(X_test, y_test)
+
+
+
