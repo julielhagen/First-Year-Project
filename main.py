76bc300f081187a528fb5d8d8d5ebc1369231423
Hugo Hoydal <hugohoydal@hotmail.com>
2023-05-17 12:40:50 +0200

    Added pics

--- main.py
+++ main.py
@@ -18,9 +18,11 @@ image_folder = 'images' + os.sep
 mask_folder = 'images_masks' + os.sep
 file_features = 'feature_data.csv'
 
-feature_names = ['assymmetry', 'red_var', 'green_var', 'blue_var', \
-    'hue_var', 'sat_var', 'val_var', 'dom_hue', 'dom_sat', 'dom_val', \
-    'compactness', 'convexity']
+feature_names = ['assymmetry', 'dom_hue', 'dom_sat', 'dom_val']
+
+# ['assymmetry', 'red_var', 'green_var', 'blue_var', \
+#     'hue_var', 'sat_var', 'val_var', 'dom_hue', 'dom_sat', 'dom_val', \
+#     'compactness', 'convexity']
 
 
 ### PROCESS IMAGES ###
@@ -43,17 +45,19 @@ df_features = pd.read_csv(file_features)
 
 # X and y
 X = df_features
-y =  (labels == 'BCC') | (labels == 'SCC') | (labels == 'MEL') 
+y = (labels == 'BCC') | (labels == 'SCC') | (labels == 'MEL') 
 
 
 ### TRAIN MODEL ###
 
 # Split in train test
-X_train, y_train, X_test, y_test = train_test_split(X, y, test_size = 0.2, random_state = 41, stratify = y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 41, stratify = y)
 
+
+groups = X_train['patient_id']
 X_train = X_train[feature_names]
 X_test = X_test[feature_names]
-groups = X_train['patient_id']
+
 
 # PCA
 train_pca(X_train)
@@ -63,8 +67,8 @@ X_train_transformed = apply_pca(X_train)
 classifiers = [KNeighborsClassifier(n_neighbors=i) for i in range(1, 20, 2)]
 
 # Cross Validate
-cv_results = cross_validate(X_train_transformed, y, classifiers, groups)
-print_results(evaluation_results)
+cv_results = cross_validate_clf(X_train, y_train, classifiers, groups)
+print_results(cv_results)
 
 # Train classifier
 #trained_classifiers = train_clf(X_train, y_train, classifiers)
